e54afa07bfa2a93e7a2892cf4cdf9721969762ca
Piotr Szotkowski <chastell@chastell.net>
2015-12-30 18:11:10 +0100

    test Lovely.wrap’s default width

--- spec/lovely/lovely_spec.cr
+++ spec/lovely/lovely_spec.cr
@@ -3,8 +3,11 @@ require "../../src/lovely"
 require "../../src/lovely/wrapper"
 
 class TestWrapper < Lovely::Wrapper
-  def wrap(text, width = 72)
-    "Ice Ice\nBaby\n" if text == "Ice Ice Baby" && width == 7
+  def wrap(text, width = raise)
+    case
+    when text == "Ice Ice Baby" && width == 7  then "Ice Ice\nBaby\n"
+    when text == "Ice Ice Baby" && width == 72 then "Ice Ice Baby\n"
+    end
   end
 end
 
@@ -13,6 +16,11 @@ describe Lovely do
     it "offloads the wrapping to a wrapper" do
       wrapped = Lovely.wrap("Ice Ice Baby", width: 7, wrapper: TestWrapper.new)
       wrapped.should eq("Ice Ice\nBaby\n")
+    end
+
+    it "wraps the passed String to 72 characters by default" do
+      wrapped = Lovely.wrap("Ice Ice Baby", wrapper: TestWrapper.new)
+      wrapped.should eq("Ice Ice Baby\n")
     end
   end
 end
