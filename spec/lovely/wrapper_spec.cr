cfdb64da8c6ea7662698874847e388eba5d5052f
Piotr Szotkowski <chastell@chastell.net>
2016-11-25 09:43:45 +0100

    more specs! \o/

--- spec/lovely/wrapper_spec.cr
+++ spec/lovely/wrapper_spec.cr
@@ -15,6 +15,23 @@ module Lovely
         __(Wrapper.new.call(short, 72)).must_equal "#{short}\n"
         __(Wrapper.new.call(long, 40)).must_equal wrap
       end
+
+      it "wraps the passed String to the given number of characters" do
+        input = "something grabs a hold of me tightly; " \
+          "flow like a harpoon – daily and nightly"
+        __(Wrapper.new.call(input, width: 40)).must_equal <<-end
+          something grabs a hold of me tightly;
+          flow like a harpoon – daily and nightly
+
+          end
+        __(Wrapper.new.call(input, width: 21)).must_equal <<-end
+          something grabs a
+          hold of me tightly;
+          flow like a harpoon –
+          daily and nightly
+
+          end
+      end
     end
   end
 end
