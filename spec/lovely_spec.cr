30a20ebb01bb181697d25e39010f249a7d54a6cf
Piotr Szotkowski <chastell@chastell.net>
2016-12-01 23:40:55 +0100

    Lovely spec: let’s not mock out the wrapper for now

--- spec/lovely_spec.cr
+++ spec/lovely_spec.cr
@@ -1,26 +1,15 @@
 require "./spec_helper"
 require "../src/lovely"
-require "../src/lovely/wrapper"
-
-Mocks.create_mock(Lovely::Wrapper) do
-  mock call(text, width)
-end
 
 describe Lovely do
   describe ".wrap" do
-    it "offloads the wrapping to a wrapper" do
-      wrapper = Mocks.instance_double(Lovely::Wrapper,
-                                      returns(call("Ice Ice Baby", 7),
-                                              "Ice Ice\nBaby\n"))
-      wrapped = Lovely.wrap("Ice Ice Baby", width: 7, wrapper: wrapper)
+    it "wraps a string to the given width" do
+      wrapped = Lovely.wrap("Ice Ice Baby", width: 7)
       __(wrapped).must_equal("Ice Ice\nBaby\n")
     end
 
-    it "wraps the passed String to 72 characters by default" do
-      wrapper = Mocks.instance_double(Lovely::Wrapper,
-                                      returns(call("Ice Ice Baby", 72),
-                                              "Ice Ice Baby\n"))
-      wrapped = Lovely.wrap("Ice Ice Baby", wrapper: wrapper)
+    it "wraps a string to 72 characters by default" do
+      wrapped = Lovely.wrap("Ice Ice Baby")
       __(wrapped).must_equal("Ice Ice Baby\n")
     end
   end
