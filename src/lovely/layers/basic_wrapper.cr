d7442deac1247adbcc1fa339b66b78ca15200d4c
Piotr Szotkowski <chastell@chastell.net>
2016-12-06 00:11:35 +0100

    actually nest BasicWrapper under Lovely::Layers

--- src/lovely/layers/basic_wrapper.cr
+++ src/lovely/layers/basic_wrapper.cr
@@ -1,10 +1,14 @@
-class BasicWrapper
-  getter :text, :width
+module Lovely
+  module Layers
+    class BasicWrapper
+      getter :text, :width
 
-  def initialize(@text : String, @width = 72)
-  end
+      def initialize(@text : String, @width = 72)
+      end
 
-  def call
-    text.tr("\n", " ").strip.gsub(/(.{1,#{width}})( |$\n?)/, "\\1\n")
+      def call
+        text.tr("\n", " ").strip.gsub(/(.{1,#{width}})( |$\n?)/, "\\1\n")
+      end
+    end
   end
 end
--- src/lovely/wrapper.cr
+++ src/lovely/wrapper.cr
@@ -3,7 +3,7 @@ require "./layers/basic_wrapper"
 module Lovely
   class Wrapper
     def call(text, width = 72)
-      BasicWrapper.new(text, width: width).call
+      Layers::BasicWrapper.new(text, width: width).call
     end
   end
 end
