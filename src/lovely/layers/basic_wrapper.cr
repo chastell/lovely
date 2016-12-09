b22ee8e6fc98cb2244507b8fdbba4da6e840e380
Piotr Szotkowski <chastell@chastell.net>
2016-12-10 00:36:31 +0100

    drop BasicWrapper getters

--- src/lovely/layers/basic_wrapper.cr
+++ src/lovely/layers/basic_wrapper.cr
@@ -1,8 +1,6 @@
 module Lovely
   module Layers
     class BasicWrapper
-      getter :text, :width
-
       def call(text, width = 72)
         text.tr("\n", " ").strip.gsub(/(.{1,#{width}})( |$\n?)/, "\\1\n")
       end
