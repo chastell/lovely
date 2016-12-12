d2f28cf600c16ef3d11dfd9106152046ab6b7cab
Piotr Szotkowski <chastell@chastell.net>
2016-12-13 00:20:30 +0100

    BasicWrapper: switch to a (private) getter

--- src/lovely/layers/basic_wrapper.cr
+++ src/lovely/layers/basic_wrapper.cr
@@ -1,14 +1,14 @@
 module Lovely
   module Layers
     class BasicWrapper
-      @next_layer : String, Int32 -> String
+      private getter next_layer : String, Int32 -> String
 
       def initialize(@next_layer = -> (text : String, width : Int32) { text })
       end
 
       def call(text, width = 72)
         wrap = text.tr("\n", " ").strip.gsub(/(.{1,#{width}})( |$\n?)/, "\\1\n")
-        @next_layer.call(wrap.chomp, width)
+        next_layer.call(wrap.chomp, width)
       end
     end
   end
