1b078635cf5721bcb2bfe5ff510d7edae0ef3dd7
Piotr Szotkowski <chastell@chastell.net>
2016-12-17 03:36:42 +0100

    Layer: actually allow Layers for next_layer

--- src/lovely/layers/layer.cr
+++ src/lovely/layers/layer.cr
@@ -1,9 +1,13 @@
 module Lovely
   module Layers
     class Layer
-      private getter next_layer : String, Int32 -> String
+      private getter next_layer : Layer | (String, Int32 -> String)
 
       def initialize(@next_layer = -> (text : String, width : Int32) { text })
+      end
+
+      def call(text, width)
+        next_layer.call(text, width)
       end
     end
   end
