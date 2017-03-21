d2d66d453986a03e5c6756e1712500b64189652b
Piotr Szotkowski <chastell@chastell.net>
2017-03-21 06:30:48 +0100

    HangoutWrapper: drop last not_nil!

--- src/lovely/layers/hangout_wrapper.cr
+++ src/lovely/layers/hangout_wrapper.cr
@@ -51,7 +51,8 @@ module Lovely
           return true unless cut = upper.chomp.rindex(/\p{Zs}/)
           upper_after = upper[0...cut] + "\n"
           lower_after = upper[(cut + 1)..-1] + lower
-          lower_after.chomp.rindex(/\p{Zs}/).not_nil! <= upper_after.size
+          return true unless last_space = lower_after.chomp.rindex(/\p{Zs}/)
+          last_space <= upper_after.size
         end
       end
     end
