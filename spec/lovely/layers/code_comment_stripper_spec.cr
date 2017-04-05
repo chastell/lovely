e145067349dbbfa07282970bb1548df74ef31246
Piotr Szotkowski <chastell@chastell.net>
2017-04-05 21:20:16 +0200

    first cut of CodeCommentStripper

--- spec/lovely/layers/code_comment_stripper_spec.cr
+++ spec/lovely/layers/code_comment_stripper_spec.cr
@@ -25,6 +25,18 @@ module Lovely
           text = "# Ice # Ice # Baby"
           CodeCommentStripper.new.call(text, width: 72).should eq text
         end
+
+        it "strips // code comments (and adds them back before returning)" do
+          commented = <<-end
+            // so fast other DJs say ‘damn!’
+            // if my rhyme was a drug I’d sell it by the gram
+            end
+          sparked = <<-end
+            // ✨ so fast other DJs say ‘damn!’
+            // if my rhyme was a drug I’d sell it by the gram ✨
+            end
+          CodeCommentStripper.new(Spark).call(commented, 72).should eq sparked
+        end
       end
     end
   end
--- src/lovely/layers/code_comment_stripper.cr
+++ src/lovely/layers/code_comment_stripper.cr
@@ -3,6 +3,21 @@ require "./layer"
 module Lovely
   module Layers
     class CodeCommentStripper < Layer
+      QUOTES = /^\s*(\/\/|#) /
+
+      def call(text, width)
+        wrapped = next_layer.call(stripped(text), width - quote(text).size)
+        wrapped.lines.map { |line| quote(text) + line }.join("\n")
+      end
+
+      private def quote(text)
+        starts = text.lines.map { |line| line[QUOTES]? }.uniq
+        starts.size == 1 ? starts.first || "" : ""
+      end
+
+      private def stripped(text)
+        text.lines.map { |line| line[quote(text).size..-1] }.join("\n")
+      end
     end
   end
 end
