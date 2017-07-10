cfa07687594ecd22654a2f019feed0dd0997b0de
Piotr Szotkowski <chastell@chastell.net>
2017-07-11 00:35:53 +0200

    Fix what `crystal tool format` broke

--- spec/lovely/wrapper_spec.cr
+++ spec/lovely/wrapper_spec.cr
@@ -14,9 +14,9 @@ module Lovely
         short = "all right: stop, collaborate and listen"
         long = short + " – Ice is back with a brand new invention"
         wrap = <<-end
-         all right: stop, collaborate and listen
-         – Ice is back with a brand new invention
-         end
+          all right: stop, collaborate and listen
+          – Ice is back with a brand new invention
+          end
         Wrapper.new.call(short).should eq short
         Wrapper.new.call(long, width: 40).should eq wrap
       end
