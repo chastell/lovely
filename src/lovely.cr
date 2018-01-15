77e665f24f628ffac081614a81b5d0d4e3fdc1c1
Piotr Szotkowski <chastell@chastell.net>
2018-01-15 20:17:52 +0100

    Backport a String#tr fix for Crystal 0.24.1

--- src/lovely.cr
+++ src/lovely.cr
@@ -5,3 +5,19 @@ module Lovely
     wrapper.call(text, width: width)
   end
 end
+
+# backport of https://github.com/crystal-lang/crystal/pull/5350
+class String
+  private def gsub_ascii_char(char, replacement)
+    String.new(bytesize) do |buffer|
+      to_slice.each_with_index do |byte, i|
+        if char.ord == byte
+          buffer[i] = replacement.ord.to_u8
+        else
+          buffer[i] = byte
+        end
+      end
+      {bytesize, bytesize}
+    end
+  end
+end
