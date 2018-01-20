fba3cd037edeca0245c70737604d44e36e416ebb
Piotr Szotkowski <chastell@chastell.net>
2018-01-21 00:58:21 +0100

    Switch from Cake to Sam

--- src/sam.cr
+++ src/sam.cr
@@ -1,7 +1,11 @@
-task :build do
+require "sam"
+
+Sam.task "build" do
   `crystal build --link-flags='-static' --no-debug -o bin/lovely --release bin/lovely.cr`
 end
 
-task :format do
+Sam.task "format" do
   `crystal tool format`
 end
+
+Sam.help
